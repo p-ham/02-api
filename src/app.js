305d59974122dc1bd0eb611188bd57593862b19b
Philipp Hamerle <phamerle.mmt-b2016@fh-salzburg.ac.at>
2018-04-10 11:05:03 +0200

    add: working router with api

--- src/app.js
+++ src/app.js
@@ -4,13 +4,15 @@ import Handlebars from 'hbsfy/runtime'
 
 // templates
 import homeTpl from './templates/home.hbs'
-import playersTpl from './templates/players.hbs'
-import playerTpl from './templates/player.hbs'
+import driversTpl from './templates/drivers.hbs'
+import driverTpl from './templates/driver.hbs'
+import constructorsTpl from './templates/constructors.hbs'
+import constructorTpl from './templates/constructor.hbs'
 import contactTpl from './templates/contact.hbs'
 import notFoundTpl from './templates/not-found.hbs'
 
 // json
-import playersJson from './players.json'
+// import playersJson from './players.json'
 
 Handlebars.registerHelper('age', function(date){
   return 2018 - parseInt(date.substr(0,4))
@@ -26,17 +28,79 @@ function contact() {
   $app.html(contactTpl())
 }
 
-function player(context){
-  const pSlug = context.params.player
-  const match = playersJson.players.find(function(e) {
-    if(e.slug === pSlug) return e
+function driver(context){
+  const pSlug = context.params.driverID
+  let url = "http://ergast.com/api/f1/drivers/" + pSlug + ".json"
+  let urlConst = "http://ergast.com/api/f1/drivers/" + pSlug + "/constructors.json"
+  let urlResults = "http://ergast.com/api/f1/drivers/" + pSlug + "/results.json"
+  let obj = {}
+  fetch(url)
+  .then(function(response) {
+    return response.json();
   })
-  console.log(match)
-  $app.html(playerTpl(match))
+  .then(function(myJson) {
+    obj = myJson.MRData.DriverTable.Drivers[0]
+
+    fetch(urlConst)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      obj.constructors = myJson.MRData.ConstructorTable.Constructors
+
+      fetch(urlResults)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        obj.races = myJson.MRData.RaceTable.Races
+        $app.html(driverTpl(obj))
+      })
+
+    })
+  });
 }
 
-function players(){
-  $app.html(playersTpl(playersJson))
+function drivers(){
+  fetch('http://ergast.com/api/f1/drivers.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    $app.html(driversTpl(myJson.MRData.DriverTable))
+  });
+}
+
+function constructor(context){
+  const pSlug = context.params.constructorID
+  let url = "http://ergast.com/api/f1/constructors/" + pSlug + ".json"
+  let urlDrivers = "http://ergast.com/api/f1/constructors/" + pSlug + "/drivers.json"
+  let obj = {}
+  fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    obj = myJson.MRData.ConstructorTable.Constructors[0]
+    fetch(urlDrivers)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      obj.drivers = myJson.MRData.DriverTable.Drivers
+      $app.html(constructorTpl(obj))
+    })
+  });
+}
+
+function constructors(){
+  fetch('http://ergast.com/api/f1/constructors.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    $app.html(constructorsTpl(myJson.MRData.ConstructorTable))
+  });
 }
 
 function notFound() {
@@ -44,8 +108,10 @@ function notFound() {
 }
 
 router('/', index)
-router('/players/:player', player)
-router('/players', players)
+router('/drivers/:driverID', driver)
+router('/drivers', drivers)
+router('/constructor/:constructorID', constructor)
+router('/constructors', constructors)
 router('/contact', contact)
 router('*', notFound)
 router()
